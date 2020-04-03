69dc8a862b829ab2f508a82adf649f85b50510ce
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 13:06:08 +0100

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('admin/user', 'AdminUsersController');
+
+Route::get('/admin', function(){
+    return view('admin.index');
+});
